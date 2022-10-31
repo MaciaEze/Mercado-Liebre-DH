392c21ec168c8dae4136451cbe4fb0cf19d51474
MaciaEze <88708633+MaciaEze@users.noreply.github.com>
2022-10-30 23:44:29 -0300

    rearmando pagina

---
+++ app.js
@@ -0,0 +1,32 @@
+//requires
+const express=require('express');
+const app=express();
+const path=require('path')
+
+//llamado a public
+app.use(express.static('public'));
+
+//llamado al puerto
+PORT = 3030;
+app.listen(PORT, ()=>console.log("Servidor escuchando en el puerto " + PORT))
+
+
+//llamado al home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+});
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+
+
+//error 404
+app.get('*', (req, res) => {
+    res.status(404).send("Ruta no encontrada")
+})
